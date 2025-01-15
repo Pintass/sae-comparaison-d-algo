e0afce32851019ec509854b24ecaef36fc938d91
Pintass <dr8050211@gmail.com>
2025-01-15 23:08:17 +0100

    début du code avec parcours_largeur

--- IA/IA_aleatoire.py
+++ IA/IA_aleatoire.py
@@ -19,6 +19,49 @@ class IA_Bomber:
     pass
 
 
+    def parcours_largeur(self, map: list, sommet_depart: tuple):
+        """
+        parcours_largeur est une fonction permettant de trouver le chemin le plus rapide vers une position donnée
+
+        ##A CHANGER ##A CHANGER ##A CHANGER ##A CHANGER ##A CHANGER 
+        Args: 
+        self (class)
+        map (list): représente la map sous forme de list.
+        sommet_depart(tuple): représente les coordonnées de départ sur l'axe x et y du joueur. 
+        """
+        distance = {}
+        distance[sommet_depart] = 0
+        pred = {}
+        attente = []  #file d'attente
+        attente.append(sommet_depart)
+        
+        while len(attente) > 0:
+            courant = attente.pop(0) #sommet dont on va chercher les voisins
+            
+            #chercher les voisins de courant
+            voisins_possibles = [(courant[0]+1,courant[1]), (courant[0]-1,courant[1]), (courant[0],courant[1]+1), (courant[0],courant[1]-1) ]
+            for v in voisins_possibles:
+                if map[v[0]][v[1]] == ' ' : #si c'est une case vide
+                    if v not in distance :  #si v est encore inconnu
+                        # dans ce cas v est une case voisine vide et inconnue
+                        distance[v] = distance[courant] + 1
+                        pred[v] = courant
+                        attente.append(v)  #v devra devenir courant plus tard
+        """ATTENTION : actuellement la fonction ne trouve pas le point que l'on souhaite mais le fait de parcourir toute la map le plus rapidement possible, il faudrait adapter cela : 
+        
+        chemin = []
+        arrivée = (3,19)
+
+        c = arrivée
+        while c != (1,1):
+            chemin.append(c)
+            c = pred[c]
+        chemin.append((1,1))
+        print(chemin)
+        permettant de définir notre arrivée cependant ATTENTION UNE FONCTION NE DOIT PAS FAIRE PLUS DE 15-20 LIGNES"""                
+    
+    return distance, pred
+
     def action(self, game_dict : dict) -> str:
         """Appelé à chaque décision du joueur IA
 
